a70a352dee8881e0edbc6bcbae682fbb1d6c21dd
Suada <selimovicsuki@gmail.com>
2025-06-23 20:37:25 +0200

    Make cards (greek salad, bruchetta,  lemon dessert) dynamic with pros

--- src/pages/home/Highlights.jsx
+++ src/pages/home/Highlights.jsx
@@ -1,68 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
-import GreekSalad from "../../assets/greek salad.jpg";
-import Bruchetta from "../../assets/bruchetta.svg";
-import LemonDessert from "../../assets/lemon dessert.jpg";
-
 import "./Highlights.css";
 
-const Highlights = () => {
+const Highlights = ({ specials }) => {
   return (
     <section className="highlights" id="specials">
       <div className="section-header">
         <h2>This weeks specials!</h2>
       </div>
       <div className="specials-grid">
-        {/* Greek Salad */}
-        <article className="special">
-          <img src={GreekSalad} alt="Greek Salad" />
-          <div>
-            <h3>
-              Greek Salad <span>$12.99</span>
-            </h3>
-            <p>
-              The famous greek salad of crispy lettuce, peppers, olives and our
-              Chicago style feta cheese, garnished with crunchy garlic and
-              rosemary croutons.
-            </p>
-            <Link to="/order">Order a delivery 🚴</Link>
-          </div>
-        </article>
-
-        {/* Bruschetta */}
-        <article className="special">
-          <img src={Bruchetta} alt="Bruschetta" />
-          <div>
-            <h3>
-              Bruschetta <span>$5.89</span>
-            </h3>
-            <p>
-              Our Bruschetta is made from grilled bread that has been smeared
-              with garlic and seasoned with salt and olive oil.
-            </p>
-            <Link to="/order">Order a delivery 🚴</Link>
-          </div>
-        </article>
-
-        {/* Lemon Dessert */}
-        <article className="special">
-          <img src={LemonDessert} alt="Lemon Dessert" />
-          <div>
-            <h3>
-              Lemon Dessert <span>$5.00</span>
-            </h3>
-            <p>
-              This comes straight from grandma’s recipe book, every last
-              ingredient has been sourced and is as authentic as can be
-              imagined.
-            </p>
-            <Link to="/order">Order a delivery 🚴</Link>
-          </div>
-        </article>
+        {specials.map((special) => (
+          <article className="special" key={special.title}>
+            <img src={special.image} alt={special.title} />
+            <div>
+              <h3>
+                {special.title} <span>{special.price}</span>
+              </h3>
+              <p>{special.description}</p>
+              <Link to={special.link}>Order a delivery 🚴</Link>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
+};
+
+Highlights.propTypes = {
+  specials: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      price: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Highlights;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,12 +7,43 @@ import Testimonials from "./Testimonials";
 import About from "./About";
 import Footer from "../../components/Footer";
 
+import GreekSalad from "../../assets/greek salad.jpg";
+import Bruchetta from "../../assets/bruchetta.svg";
+import LemonDessert from "../../assets/lemon dessert.jpg";
+
+const specials = [
+  {
+    title: "Greek Salad",
+    price: "$12.99",
+    description:
+      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
+    image: GreekSalad,
+    link: "/order",
+  },
+  {
+    title: "Bruschetta",
+    price: "$5.89",
+    description:
+      "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+    image: Bruchetta,
+    link: "/order",
+  },
+  {
+    title: "Lemon Dessert",
+    price: "$5.00",
+    description:
+      "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+    image: LemonDessert,
+    link: "/order",
+  },
+];
+
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
-      <Highlights />
+      <Highlights specials={specials} />
       <Testimonials />
       <About />
       <Footer />
