3f708b52491dd6b0b395467bf0dced865e8665ab
Suada <selimovicsuki@gmail.com>
2025-06-25 19:43:28 +0200

    Add Route for HomePage with path="/" for deployment purposes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <Router basename="/little-lemon-home-booking">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/booking" element={<Booking />} />
         </Routes>
